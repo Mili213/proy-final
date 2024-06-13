20f16f38c879adb72ecc7126078c33084a16a30d
Mili213 <milivillafane3312@gmail.com>
2024-06-13 13:49:16 -0300

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import NavBar from './nav/NavBar';
 import { Container, Typography } from '@mui/material';
 import './App.css';
 import TodoList from './To-do/TodoList';
-import { useTodo } from './hooks/useTodo';
 
 const App = () => {
-  const {
-    todos,
-    todosCount,
-    pendingTodosCount,
-    handleNewTodo,
-    handleDeleteTodo,
-    handleCompleteTodo,
-    handleUpdateTodo
-  } = useTodo();
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        setTodos(data);
+      } catch (error) {
+        console.error('Error fetching todos:', error);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter(todo => !todo.completed).length;
+
+  const handleNewTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleCompleteTodo = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleUpdateTodo = (updatedTodo) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === updatedTodo.id ? updatedTodo : todo
+      )
+    );
+  };
 
   return (
     <>
-       <Container style={{ padding: '5px', backgroundColor: '#f5f5f5' }}>
-        <Typography variant="p" component="div" gutterBottom style={{ color: '#333' }}>
+      <Container style={{ padding: '5px', backgroundColor: '#f5f5f5' }}>
+        <Typography variant="body1" component="div" gutterBottom style={{ color: '#333' }}>
           N° Tareas: {todosCount}
         </Typography>
-        <Typography variant="p" component="div" gutterBottom style={{ color: '#333' }}>
+        <Typography variant="body1" component="div" gutterBottom style={{ color: '#333' }}>
           Pendientes: {pendingTodosCount}
         </Typography>
       </Container>
--- src/nav/NavBar.jsx
+++ src/nav/NavBar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import './NavBar.css'
+import { MenuItem, Menu, Button} from '@mui/material';
 
 const NavBar = () => {
+  const [menuA, setMenuA]= React.useState(null);
+  const open = Boolean(menuA);
+
+  const handleClick=(event)=>{
+    setMenuA(event.currentTarget);
+  };
+const handleClose=()=>{
+  setMenuA(null)
+}
+
   return (
     <div className='Nav'>
-    <button className='ButtonNav'>Categorias</button>
-    <button className='ButtonNav'>Estado</button>
-    <button className='ButtonNav'>Fav</button>
+      <Button
+       className='ButtonNav'
+       aria-controls='menu-nav'
+       aria-haspopup='true'
+       onClick={handleClick}
+       >
+        Menú
+      </Button>
+      <Menu
+      id='menu-nav'
+      anchorEl={menuA}
+      open={open}
+      onClose={handleClose}
+      anchorOrigin={{vertical: 'top',horizontal:'right',}}
+      transformOrigin={{vertical:'top', horizontal:'right',}}
+> 
+    <MenuItem onClick={handleClose}>Categorias</MenuItem>
+    <MenuItem onClick={handleClose}>Estado</MenuItem>
+    <MenuItem onClick={handleClose}>Fav</MenuItem>
+    </Menu>
     </div>
   )
 }
